5753a0d921e60dbf57c8b2f8e965d7cd85c8084a
Your Name <you@example.com>
2016-07-16 20:43:31 +0100

    1st commit

---
+++ lib/recordx_sqlite.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+# file: recordx_sqlite.rb
+
+require 'sqlite3'
+require 'recordx'
+
+
+class RecordxSqlite
+
+  def initialize(dbfile, table)
+
+    @db = SQLite3::Database.new dbfile
+    @db.results_as_hash = true
+    rs = @db.query 'select * from ' + table
+
+    @a = rs.map do |h| 
+      h2 = h.inject({}) {|r, x| k, v = x; r.merge(k.to_sym => v)}
+      RecordX.new(h2, self, h2[:uid]) 
+    end
+
+  end
+
+  def all()
+    @a
+  end
+
+  def update(id, h={})
+
+    col, value = h.to_a.first
+    return if col == :id
+
+s = "
+UPDATE headers
+SET #{col}='#{value}'
+WHERE id='#{id}';"
+
+    @db.execute(s)
+
+  end
+end
+
+
+r = rs.all.first
---
+++ recordx_sqlite.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'recordx_sqlite'
+  s.version = '0.1.0'
+  s.summary = 'RecordX-SQLite is an object relational mapper primarily designed for updating records in bulk.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/recordx_sqlite.rb']
+  s.add_runtime_dependency('sqlite3', '~> 1.3', '>=1.3.11')
+  s.add_runtime_dependency('recordx', '~> 0.4', '>=0.4.0')
+  s.signing_key = '../privatekeys/recordx_sqlite.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/recordx_sqlite'
+end
