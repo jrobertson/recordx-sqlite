2144757ddd147192729f6c1c79e9e0f1c7ee7dd7
Your Name <you@example.com>
2016-07-17 13:26:55 +0100

    feature: customised SQL can now be used.

--- lib/recordx_sqlite.rb
+++ lib/recordx_sqlite.rb
@@ -8,29 +8,42 @@ require 'recordx'
 
 class RecordxSqlite
 
-  def initialize(dbfile, table: '', primary_key: :id, pk: primary_key)
-    
-    
+  def initialize(dbfile, table: '', primary_key: :id, pk: primary_key, 
+                 sql: 'select * from ' + table)
+        
     @db = SQLite3::Database.new dbfile
 
     @db.results_as_hash = true
-    rs = @db.query 'select * from ' + table
-
-    @table, @primary_key = table, pk.to_sym
-    
-    @a = rs.map do |h| 
-      h2 = h.inject({}) {|r, x| k, v = x; r.merge(k.to_sym => v)}
-      RecordX.new(h2, self, h2[@primary_key]) 
-    end
+    @table, @primary_key, @sql = table, pk.to_sym, sql
+    @a = nil
     
   end
 
-  def all()
+  def all()    
+    query(@sql) unless @a
     @a
   end
   
   def find(id)
+    query(@sql) unless @a
     @a.find {|x| x.method(@primary_key).call == id}
+  end
+  
+  def query(sql=@sql)
+    
+    @sql = sql
+    rs = @db.query sql
+    
+    @a = rs.map do |h| 
+      h2 = h.inject({}) {|r, x| k, v = x; r.merge(k.to_sym => v)}
+      RecordX.new(h2, self, h2[@primary_key]) 
+    end    
+    
+  end  
+  
+  def refresh()
+    query(@sql)
+    'refreshed'
   end
 
   def update(id, h={})
@@ -46,5 +59,6 @@ WHERE #{@primary_key.to_s}='#{id}';"
     @db.execute(s)
 
   end
+    
 end
 
